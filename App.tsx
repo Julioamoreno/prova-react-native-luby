799778283611667b130535face6371a4d73810cd
Julio <julioarmando321@gmail.com>
2021-06-22 19:23:31 -0300

    feat: add authentication screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Auth from './screens/Auth';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+type RootStackParamList = {
+	Auth: undefined;
+};
+
+const RootStack = createStackNavigator<RootStackParamList>();
+
+const App: React.FC<{}> = () => {
+	return (
+		<NavigationContainer>
+			<RootStack.Navigator
+				initialRouteName='Auth'
+				screenOptions={{ headerShown: false }}
+			>
+				<RootStack.Screen name='Auth' component={Auth} />
+			</RootStack.Navigator>
+		</NavigationContainer>
+	);
+};
+export default App;
